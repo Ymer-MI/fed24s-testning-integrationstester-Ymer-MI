029ffba570ba0007e04ea3547965c135cdba938f
ymer94 <ymer_94@hotmail.com>
2025-09-15 15:30:40 +0200

    Updated test descriptions.
    Changes to be committed:
    §	modified:   src/__tests__/functions.test.ts

--- src/__tests__/functions.test.ts
+++ src/__tests__/functions.test.ts
@@ -13,14 +13,14 @@ describe('Testing helper functions', () => {
         movies = [{'Title':'The Lord of the Rings: The Fellowship of the Ring'},{'Title':'The Lord of the Rings: The Return of the King'},{'Title':'The Lord of the Rings: The Two Towers'},{'Title':'The Lord of the Rings: The Rings of Power'},{'Title':'The Lord of the Rings'},{'Title':'The Lord of the Rings: The War of the Rohirrim'},{'Title':'The Lord of the Rings: The Two Towers'},{'Title':'The Lord of the Rings: The Return of the King'},{'Title':'The Lord of the Rings: The Battle for Middle-Earth'},{'Title':'The Lord of the Rings: The Battle for Middle-Earth II'}] as IMovie[]
     })
 
-    test('movieSort sorts movies by title in descending order by default', () => {
+    test('movieSort sorts movies by title in ascending order by default', () => {
         const sortedMovies = movieSort(movies)
 
         expect(sortedMovies[0].Title).toBe(Title.A)
         expect(sortedMovies[sortedMovies.length - 1].Title).toBe(Title.Z)
     })
 
-    test('movieSort sorts movies by title in ascending order when desc is false', () => {
+    test('movieSort sorts movies by title in descending order when desc is false', () => {
         const sortedMovies = movieSort(movies, false)
 
         expect(sortedMovies[0].Title).toBe(Title.Z)
