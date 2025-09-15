27dbb3086f6d78b5d0c9a3eca58f1f696ea26308
ymer94 <ymer_94@hotmail.com>
2025-09-15 15:12:44 +0200

    First test created.
    Changes to be committed:
    	modified:   src/__tests__/functions.test.ts
    	modified:   src/__tests__/movieApp.test.ts
    	modified:   src/__tests__/movieService.test.ts

--- src/__tests__/functions.test.ts
+++ src/__tests__/functions.test.ts
@@ -1,3 +1,12 @@
+import { IMovie } from '../ts/models/Movie'
+import { movieSort } from '../ts/functions'
+
 describe('Testing helper functions', () => {
-    
+    test('movieSort sorts movies by title in descending order by default', () => {
+        const moveies = [{'Title':'The Lord of the Rings: The Fellowship of the Ring'},{'Title':'The Lord of the Rings: The Return of the King'},{'Title':'The Lord of the Rings: The Two Towers'},{'Title':'The Lord of the Rings: The Rings of Power'},{'Title':'The Lord of the Rings'},{'Title':'The Lord of the Rings: The War of the Rohirrim'},{'Title':'The Lord of the Rings: The Two Towers'},{'Title':'The Lord of the Rings: The Return of the King'},{'Title':'The Lord of the Rings: The Battle for Middle-Earth'},{'Title':'The Lord of the Rings: The Battle for Middle-Earth II'}] as IMovie[],
+        sortedMovies = movieSort(moveies)
+
+        expect(sortedMovies[0].Title).toBe('The Lord of the Rings')
+        expect(sortedMovies[sortedMovies.length - 1].Title).toBe('The Lord of the Rings: The War of the Rohirrim')
+    })
 })
---
+++ src/__tests__/movieApp.test.ts
@@ -0,0 +1,1 @@
+describe('testing movie app functions', () => {})
