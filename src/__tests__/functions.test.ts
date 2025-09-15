0305651eb84935e06df52f265f8c3edf2fcc84af
ymer94 <ymer_94@hotmail.com>
2025-09-15 15:24:28 +0200

    Secund test added.
    Changes to be committed:
    	modified:   package.json
    	modified:   src/__tests__/functions.test.ts

--- src/__tests__/functions.test.ts
+++ src/__tests__/functions.test.ts
@@ -1,12 +1,29 @@
 import { IMovie } from '../ts/models/Movie'
 import { movieSort } from '../ts/functions'
 
-describe('Testing helper functions', () => {
-    test('movieSort sorts movies by title in descending order by default', () => {
-        const moveies = [{'Title':'The Lord of the Rings: The Fellowship of the Ring'},{'Title':'The Lord of the Rings: The Return of the King'},{'Title':'The Lord of the Rings: The Two Towers'},{'Title':'The Lord of the Rings: The Rings of Power'},{'Title':'The Lord of the Rings'},{'Title':'The Lord of the Rings: The War of the Rohirrim'},{'Title':'The Lord of the Rings: The Two Towers'},{'Title':'The Lord of the Rings: The Return of the King'},{'Title':'The Lord of the Rings: The Battle for Middle-Earth'},{'Title':'The Lord of the Rings: The Battle for Middle-Earth II'}] as IMovie[],
-        sortedMovies = movieSort(moveies)
+enum Title {
+    A = 'The Lord of the Rings',
+    Z = 'The Lord of the Rings: The War of the Rohirrim'
+}
 
-        expect(sortedMovies[0].Title).toBe('The Lord of the Rings')
-        expect(sortedMovies[sortedMovies.length - 1].Title).toBe('The Lord of the Rings: The War of the Rohirrim')
+describe('Testing helper functions', () => {
+    let movies: IMovie[] = []
+
+    beforeEach(() => {
+        movies = [{'Title':'The Lord of the Rings: The Fellowship of the Ring'},{'Title':'The Lord of the Rings: The Return of the King'},{'Title':'The Lord of the Rings: The Two Towers'},{'Title':'The Lord of the Rings: The Rings of Power'},{'Title':'The Lord of the Rings'},{'Title':'The Lord of the Rings: The War of the Rohirrim'},{'Title':'The Lord of the Rings: The Two Towers'},{'Title':'The Lord of the Rings: The Return of the King'},{'Title':'The Lord of the Rings: The Battle for Middle-Earth'},{'Title':'The Lord of the Rings: The Battle for Middle-Earth II'}] as IMovie[]
+    })
+
+    test('movieSort sorts movies by title in descending order by default', () => {
+        const sortedMovies = movieSort(movies)
+
+        expect(sortedMovies[0].Title).toBe(Title.A)
+        expect(sortedMovies[sortedMovies.length - 1].Title).toBe(Title.Z)
+    })
+
+    test('movieSort sorts movies by title in ascending order when desc is false', () => {
+        const sortedMovies = movieSort(movies, false)
+
+        expect(sortedMovies[0].Title).toBe(Title.Z)
+        expect(sortedMovies[sortedMovies.length - 1].Title).toBe(Title.A)
     })
 })
